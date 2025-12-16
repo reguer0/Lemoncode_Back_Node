8e824023f132de3b26c0c7b29a8c9a362b98398d
Gorka Reguero <reguerogorka@gmail.com>
2025-12-16 15:48:28 +0100

    Modulo3 Ejercicio 2

--- Modulo3/index.js
+++ Modulo3/index.js
@@ -42,3 +42,23 @@ function parseCSV(data) {
 parseCSV(data);
 
 
+/**
+ * Ejercicio 2
+Implementar una funcion replaceAt que tome como primer argumento un array, 
+como segundo argumento un índice y como tercer argumento un valor y reemplace el elemento 
+dentro del array en el índice indicado. El array de entrada no debe de ser mutado, eso es, 
+que debes crear un nuevo array sin modificar el existente. Utiliza spread operator, y slice para conseguirlo.
+ */
+
+
+const elements = ["lorem", "ipsum", "dolor", "sit", "amet"];
+const index = 2;
+const newValue = "furor";
+
+const replaceAt = (arr, index, newElement) => {
+    return [...arr.slice(0, index), newElement, ...arr.slice(index + 1)];
+};
+
+const result = replaceAt(elements, index, newValue);
+console.log(result === elements); // false
+console.log(result); // ['lorem', 'ipsum', 'furor', 'sit', 'amet'];
